eed5efded3a0d82f79ae21fb7f4d57adda2f9c83
Henrique Souza <henriquelbsouza@gmail.com>
2020-04-08 17:33:57 -0300

    Fixed MEDALS_TITLE for all profiles

--- profile.js
+++ profile.js
@@ -4,9 +4,7 @@ const log = (msg) => console.log('[GC Booster]', msg)
 // MEDALS BOX CONTENT STUFF
 const MEDALS_GRID = document.getElementsByClassName('gc-profile-medal-grid')[0];
 const MEDALS = MEDALS_GRID.getElementsByClassName('gc-profile-medal-grid-item');
-const MEDALS_TITLE = 
-    document.querySelector('body > div.animsition > div.body-page.bg1.noads.has-chat > div > div > div.gc-profile > div.gc-profile-container > div.gc-profile-content > div:nth-child(3) > div.gc-profile-box-header > h3')
-    || document.querySelector('body > div.animsition > div.body-page.bg1.has-chat > div > div > div.gc-profile > div.gc-profile-container > div.gc-profile-content > div:nth-child(3) > div.gc-profile-box-header > h3');
+const MEDALS_TITLE = MEDALS_GRID.parentElement.getElementsByClassName('gc-profile-title')[0];
 const SHOW_HIDE_MEDALS = '<button id="showHideMedals" style="border:1px solid;width: 110px;margin-left: 10px;">Esconder</button>'
 MEDALS_TITLE.innerHTML += ` (${MEDALS.length}) ${SHOW_HIDE_MEDALS}`;
 document.getElementById("showHideMedals").addEventListener("click", function(){
@@ -18,4 +16,3 @@ document.getElementById("showHideMedals").addEventListener("click", function(){
         document.getElementById("showHideMedals").innerText = "Mostrar";
     }
 });
-
