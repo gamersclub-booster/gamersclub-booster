f069b625154b24c27e64545a158749d02c2c995e
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2020-08-12 10:58:59 -0300

    Ban checker feature

---
+++ minhas-partidas.js
@@ -0,0 +1,51 @@
+const GC_API_URL = '';
+const SELETOR_LINK_PARTIDAS = 'a:contains("Ver partida")'
+
+const log = ( msg ) => console.log('GC BAN',  msg );
+
+const buscaLinksDasPartidas = () => {
+    let partidas = [];
+    $( SELETOR_LINK_PARTIDAS ).each( function() {
+        partidas.push( this.href ); 
+    });
+    return partidas;   
+}
+
+const verificarBans = async ( partida, matchColumn ) => {
+    try {
+        const resposta = await fetch( partida + '/1' );
+        const dadosPartida = await resposta.json();
+        const temBanidos = dadosPartida.jogos.players.team_a.some( jogador => jogador.player.banned) || dadosPartida.jogos.players.team_b.some( jogador => jogador.player.banned);
+        if ( temBanidos ) {
+            matchColumn.style.background = 'red';
+        } else {
+			matchColumn.style.background = 'green';
+
+		}
+    } catch (e) {
+        log( 'Fetch errored, trying again.' );
+        return verificarBans( partida, matchColumn );
+    }
+}
+
+// content.js
+const run = async () => {
+    const partidas = buscaLinksDasPartidas();
+    const matchColumns = $('span.versus').parent().parent();
+    const promises = partidas.map( ( partida, index ) => verificarBans( partida, matchColumns[ index ] ) );
+    await Promise.all( promises );
+}
+
+(async () => {
+    $( 'body' ).on( 'DOMNodeInserted', '#myMatchesPagination', async function() {
+        //Wait 5 seconds before start;
+        log( 'Page changed, running.' )
+        await new Promise( r => setTimeout( r, 3000 ) );
+        run();
+    });
+
+    run();
+})();
+
+
+
--- profile.js
+++ profile.js
@@ -1,18 +1,49 @@
-
+//GLOBAL STUFF
 const log = (msg) => console.log('[GC Booster]', msg)
-
-// MEDALS BOX CONTENT STUFF
-const MEDALS_GRID = document.getElementsByClassName('gc-profile-medal-grid')[0];
-const MEDALS = MEDALS_GRID.getElementsByClassName('gc-profile-medal-grid-item');
-const MEDALS_TITLE = MEDALS_GRID.parentElement.getElementsByClassName('gc-profile-title')[0];
-const SHOW_HIDE_MEDALS = '<button id="showHideMedals" style="border:1px solid;width: 110px;margin-left: 10px;">Esconder</button>'
-MEDALS_TITLE.innerHTML += ` (${MEDALS.length}) ${SHOW_HIDE_MEDALS}`;
-document.getElementById("showHideMedals").addEventListener("click", function(){
-    if (MEDALS_GRID.style.display === "none") {
-        MEDALS_GRID.style.display = "grid";
-        document.getElementById("showHideMedals").innerText = "Esconder";
-    } else {
-        MEDALS_GRID.style.display = "none";
-        document.getElementById("showHideMedals").innerText = "Mostrar";
-    }
+const profileId = +document.getElementById('GamersClubStatsBox').getAttribute('data-prop-player-id');
+//"Database"
+let watchedUsers;
+chrome.storage.sync.get(null, function (result) {
+    watchedUsers = result.watchedUsers;
+    init();
 });
+
+const init = () => {
+    // MEDALS BOX CONTENT STUFF
+    const MEDALS_GRID = document.getElementsByClassName('gc-profile-medal-grid')[0];
+    const MEDALS = MEDALS_GRID.getElementsByClassName('gc-profile-medal-grid-item');
+    const MEDALS_TITLE = MEDALS_GRID.parentElement.getElementsByClassName('gc-profile-title')[0];
+    const SHOW_HIDE_MEDALS = '<button id="gc-booster-showHideMedals" style="border:1px solid;width: 110px;margin-left: 10px;">Esconder</button>'
+    MEDALS_TITLE.innerHTML += ` (${MEDALS.length}) ${SHOW_HIDE_MEDALS}`;
+    document.getElementById("gc-booster-showHideMedals").addEventListener("click", function(){
+        if (MEDALS_GRID.style.display === "none") {
+            MEDALS_GRID.style.display = "grid";
+            document.getElementById("gc-booster-showHideMedals").innerText = "Esconder";
+        } else {
+            MEDALS_GRID.style.display = "none";
+            document.getElementById("gc-booster-showHideMedals").innerText = "Mostrar";
+        }
+    });
+
+    // PROFILE BOX CONTENT STUFF
+    const checked = watchedUsers.includes(profileId) ? 'checked' : '';
+
+    const watchSelect = document.createElement('div');
+    watchSelect.innerHTML = `<input type="checkbox" ${checked} id="gc-booster-watchCheckbox" style="display:none" class="custom-checkbox custom-form-btn-root"/>` +
+                            '<label style="color: grey; font-family: Poppins,sans-serif; font-size:10px" for="gc-booster-watchCheckbox">'+
+                            '<i class="far fa-smile-beam"></i></i></label>';
+
+    document.getElementsByClassName('main-section')[0].firstElementChild.appendChild(watchSelect);
+
+    document.getElementById("gc-booster-watchCheckbox").addEventListener("change", function(){
+        if (document.getElementById("gc-booster-watchCheckbox").checked) {
+            chrome.storage.sync.set( { watchedUsers: watchedUsers.concat( profileId ) } );
+        } else {
+            watchedUsers.splice( watchedUsers.indexOf(profileId), 1 );
+            log('olokinho mew');
+            chrome.storage.sync.set( { watchedUsers: watchedUsers } );
+        }
+    });
+}
+
+//class="game-modal-ready-button"
