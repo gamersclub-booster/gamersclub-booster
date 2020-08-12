051d27ab4eea3876cdca1017a9771e32160293a0
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2020-08-12 14:15:55 -0300

    Adds the cool and toxic guy buttons

--- profile.js
+++ profile.js
@@ -2,9 +2,13 @@
 const log = (msg) => console.log('[GC Booster]', msg)
 const profileId = +document.getElementById('GamersClubStatsBox').getAttribute('data-prop-player-id');
 //"Database"
-let watchedUsers;
+let watchedUsers = [];
+let toxicPlayers = [];
+let goodPlayers = [];
 chrome.storage.sync.get(null, function (result) {
-    watchedUsers = result.watchedUsers;
+    watchedUsers = result.watchedUsers ? result.watchedUsers : [];
+    goodPlayers = result.goodPlayers ? result.goodPlayers : [];
+    toxicPlayers = result.toxicPlayers ? result.toxicPlayers : [];
     init();
 });
 
@@ -26,22 +30,47 @@ const init = () => {
     });
 
     // PROFILE BOX CONTENT STUFF
-    const checked = watchedUsers.includes(profileId) ? 'checked' : '';
+    // const actionsDiv = document.createElement('div');
+    //Good/Nice player
+    const checkedGoodPlayer = goodPlayers.includes(profileId) ? 'checked' : '';
+    const goodPlayerColor = goodPlayers.includes(profileId) ? 'green' : '';
 
-    const watchSelect = document.createElement('div');
-    watchSelect.innerHTML = `<input type="checkbox" ${checked} id="gc-booster-watchCheckbox" style="display:none" class="custom-checkbox custom-form-btn-root"/>` +
-                            '<label style="color: grey; font-family: Poppins,sans-serif; font-size:10px" for="gc-booster-watchCheckbox">'+
-                            '<i class="far fa-smile-beam"></i></i></label>';
+    const goodPlayersSelect = document.createElement('div');
+    goodPlayersSelect.innerHTML = `<input type="checkbox" ${checkedGoodPlayer} id="gc-booster-goodPlayerCheckbox" style="" class="custom-checkbox custom-form-btn-root"/>` +
+                            '<label style="color: grey; font-family: Poppins,sans-serif; font-size:10px" for="gc-booster-goodPlayerCheckbox">'+
+                            `<i class="far fa-smile-beam" id="good-player-icon" style="font-size:30px;color:${goodPlayerColor}"></i></i></label>`;
 
-    document.getElementsByClassName('main-section')[0].firstElementChild.appendChild(watchSelect);
+    document.getElementsByClassName('main-section')[0].firstElementChild.appendChild(goodPlayersSelect);
 
-    document.getElementById("gc-booster-watchCheckbox").addEventListener("change", function(){
-        if (document.getElementById("gc-booster-watchCheckbox").checked) {
-            chrome.storage.sync.set( { watchedUsers: watchedUsers.concat( profileId ) } );
+    document.getElementById("gc-booster-goodPlayerCheckbox").addEventListener("change", function(){
+        if (document.getElementById("gc-booster-goodPlayerCheckbox").checked) {
+            document.getElementById("good-player-icon").style.color = "green";
+            chrome.storage.sync.set( { goodPlayers: goodPlayers.concat( profileId ) } );
         } else {
-            watchedUsers.splice( watchedUsers.indexOf(profileId), 1 );
-            log('olokinho mew');
-            chrome.storage.sync.set( { watchedUsers: watchedUsers } );
+            goodPlayers.splice( goodPlayers.indexOf(profileId), 1 );
+            chrome.storage.sync.set( { goodPlayers: goodPlayers } );
+            document.getElementById("good-player-icon").style.color = "";
+        }
+    });
+    //Toxic player
+    const checkedToxicPlayer = toxicPlayers.includes(profileId) ? 'checked' : '';
+    const toxicPlayerColor = toxicPlayers.includes(profileId) ? 'red' : '';
+
+    const toxicPlayersSelect = document.createElement('div');
+    toxicPlayersSelect.innerHTML = `<input type="checkbox" ${checkedToxicPlayer} id="gc-booster-toxicPlayerCheckbox" style="" class="custom-checkbox custom-form-btn-root"/>` +
+                            '<label style="color: grey; font-family: Poppins,sans-serif; font-size:10px" for="gc-booster-toxicPlayerCheckbox">'+
+                            `<i class="fas fa-biohazard" id="toxic-player-icon" style="font-size:30px;color:${toxicPlayerColor}"></i></i></label>`;
+
+    document.getElementsByClassName('main-section')[0].firstElementChild.appendChild(toxicPlayersSelect);
+
+    document.getElementById("gc-booster-toxicPlayerCheckbox").addEventListener("change", function(){
+        if (document.getElementById("gc-booster-toxicPlayerCheckbox").checked) {
+            document.getElementById("toxic-player-icon").style.color = "red";
+            chrome.storage.sync.set( { toxicPlayers: toxicPlayers.concat( profileId ) } );
+        } else {
+            toxicPlayers.splice( toxicPlayers.indexOf(profileId), 1 );
+            chrome.storage.sync.set( { toxicPlayers: toxicPlayers } );
+            document.getElementById("toxic-player-icon").style.color = "";
         }
     });
 }
