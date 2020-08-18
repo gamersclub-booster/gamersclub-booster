c1288db7c6f8d5260514836f7898a7e7854c7fd6
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2020-08-17 22:23:14 -0300

    Auto copiar IP

--- lobby.js
+++ lobby.js
@@ -1,17 +1,25 @@
 const log = ( msg ) => console.log('[GC Booster] ',  msg );
 let options = {};
-chrome.storage.sync.get(['autoAceitarPreReady'], function (result) {
+chrome.storage.sync.get(['autoAceitarPreReady', 'autoCopiarIp'], function (result) {
     options = result;
     init();
 });
 
 const init = () => {
     if ( options.autoAceitarPreReady ) {
-        const interval = setInterval(function() {
-            const button = document.getElementById('playNowOverlayReady');
-            if (button) {
-                button.click();
+        const intervalAceitar = setInterval(function() {
+            const buttonAceitar = document.getElementById('playNowOverlayReady');
+            if (buttonAceitar && buttonAceitar.textContent === 'Ready') {
+                buttonAceitar.click();
             }
         }, 5000);
     }
+    // if ( options.autoCopiarIp ) {
+        const intervalCopia = setInterval(function() {
+            const buttonCopia = document.getElementById('gameModalCopyServer');
+            if (buttonCopia && buttonCopia.textContent === 'Copiar IP') {
+                buttonCopia.click();
+            }
+        }, 5000);
+    // }
 };
