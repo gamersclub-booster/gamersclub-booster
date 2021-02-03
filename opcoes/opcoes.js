a5e4d01ba0d00d686bbda92e3911d412f7b1766d
Joao Victor Davim <joaovictor_rj@poli.ufrj.br>
2021-02-02 22:45:51 -0300

    Adiciona opcao de fixar o menu do lobby

--- opcoes/opcoes.js
+++ opcoes/opcoes.js
@@ -1,40 +1,51 @@
 function constructOptions() {
     let checkboxPreReady = document.getElementById('auto-aceitar-pre-ready');
-    
+
     let checkboxCopiarIp = document.getElementById('auto-copiar-ip');
 
     let checkboxMedalhas = document.getElementById('auto-esconder-medalhas');
-    
+
     let checkboxConquistas = document.getElementById('auto-esconder-conquistas');
 
     let checkboxReady = document.getElementById('auto-aceitar-ready');
 
-    chrome.storage.sync.get(null, function(result) {
+    let checkboxFixarMenuLobby = document.getElementById('auto-fixar-menu-lobby');
+
+    chrome.storage.sync.get(null, function (result) {
         checkboxMedalhas.checked = result.autoEsconderMedalhas;
         checkboxConquistas.checked = result.autoEsconderConquistas;
         checkboxPreReady.checked = result.autoAceitarPreReady;
         checkboxCopiarIp.checked = result.autoCopiarIp;
         checkboxReady.checked = result.autoAceitarReady;
+        checkboxFixarMenuLobby.checked = result.autoFixarMenuLobby;
     });
 
-    checkboxConquistas.addEventListener('change', function(e) {
-        chrome.storage.sync.set({autoEsconderConquistas: this.checked}, function() {});
+    checkboxConquistas.addEventListener('change', function (e) {
+        chrome.storage.sync.set({ autoEsconderConquistas: this.checked }, function () { });
     });
 
-    checkboxMedalhas.addEventListener('change', function(e) {
-        chrome.storage.sync.set({autoEsconderMedalhas: this.checked}, function() {});
+    checkboxMedalhas.addEventListener('change', function (e) {
+        chrome.storage.sync.set({ autoEsconderMedalhas: this.checked }, function () { });
     });
 
-    checkboxPreReady.addEventListener('change', function(e) {
-        chrome.storage.sync.set({autoAceitarPreReady: this.checked}, function() {});
+    checkboxPreReady.addEventListener('change', function (e) {
+        chrome.storage.sync.set({ autoAceitarPreReady: this.checked }, function () { });
     });
 
-    checkboxCopiarIp.addEventListener('change', function(e) {
-        chrome.storage.sync.set({autoCopiarIp: this.checked}, function() {});
+    checkboxCopiarIp.addEventListener('change', function (e) {
+        chrome.storage.sync.set({ autoCopiarIp: this.checked }, function () { });
     });
 
-    checkboxReady.addEventListener('change', function(e) {
-        chrome.storage.sync.set({autoAceitarReady: this.checked}, function() {});
+    checkboxReady.addEventListener('change', function (e) {
+        chrome.storage.sync.set({ autoAceitarReady: this.checked }, function () { });
+    });
+
+    checkboxFixarMenuLobby.addEventListener('change', function (e) {
+        chrome.storage.sync.set({ autoFixarMenuLobby: this.checked }, function () { });
+        chrome.tabs.getSelected(null, function (tab) {
+            var code = 'window.location.reload();';
+            chrome.tabs.executeScript(tab.id, { code: code });
+        });
     });
 }
 constructOptions();
--- pages/lobby/lobby.js
+++ pages/lobby/lobby.js
@@ -1,43 +1,49 @@
 let opcoes = {};
-chrome.storage.sync.get(['autoAceitarPreReady', 'autoCopiarIp', 'autoAceitarReady', 'autoConcordarTermosRanked'], function (result) {
+chrome.storage.sync.get(['autoAceitarPreReady', 'autoCopiarIp', 'autoAceitarReady', 'autoConcordarTermosRanked', 'autoFixarMenuLobby'], function (result) {
     opcoes = result;
     initLobby();
 });
 
 const initLobby = () => {
-    if ( opcoes.autoAceitarPreReady ) {
-        const intervalAceitar = setInterval(function() {
+    if (opcoes.autoAceitarPreReady) {
+        const intervalAceitar = setInterval(function () {
             const buttonAceitar = document.getElementById('playNowOverlayReady');
             if (buttonAceitar && buttonAceitar.textContent === 'Ready' && !buttonAceitar.disabled) {
                 buttonAceitar.click();
             }
         }, 5000);
     }
-    if ( opcoes.autoCopiarIp ) {
-        const intervalCopia = setInterval(function() {
+    if (opcoes.autoCopiarIp) {
+        const intervalCopia = setInterval(function () {
             const buttonCopia = document.getElementById('gameModalCopyServer');
             if (buttonCopia && buttonCopia.textContent === 'Copiar IP') {
                 buttonCopia.click();
             }
         }, 5000);
     }
-    if ( opcoes.autoAceitarReady ) {
-        const intervalAceitar = setInterval(function() {
+    if (opcoes.autoAceitarReady) {
+        const intervalAceitar = setInterval(function () {
             const buttonReady = $('#gameModalReadyBtn> button');
             if (buttonReady && !buttonReady.disabled) {
                 buttonReady.click();
             }
         }, 5000);
     }
+    if (opcoes.autoFixarMenuLobby) {
+        if (document.styleSheets.length == 0) {
+            document.head.appendChild(document.createElement("style"));
+        }
+        document.styleSheets[0].insertRule("#SidebarSala {position: fixed; top: 10%;}");
+    }
     //Auto concordar com termos da ranked.
     $('#rankedqualifyModal, #rankedopenModal, #rankedproModal, #rankedchallengeModal').on('transitionend', concordarTermos);
 };
 
-function concordarTermos(e) { 
-    if ( opcoes.autoConcordarTermosRanked && $('ranked-modal-agree').is(':visible')) {
+function concordarTermos(e) {
+    if (opcoes.autoConcordarTermosRanked && $('ranked-modal-agree').is(':visible')) {
         if (!['rankedqualifyModal', 'rankedopenModal', 'rankedproModal', 'rankedchallengeModal'].includes(e.target.id)) return;
         if (!e.target.classList.contains('game-modal-fade-in')) return;
         const metodo = $('.ranked-modal-agree>a').attr('onclick');
-        location.href=`${metodo}; void 0`;
+        location.href = `${metodo}; void 0`;
     }
 }
