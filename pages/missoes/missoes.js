3840c363df8a3b24b94de2504d65b834c42addec
Roberta Melo <robertameelo91@gmail.com>
2021-02-12 15:48:12 -0300

    Ajustes para opção de ocultar progresso.

--- pages/missoes/missoes.js
+++ pages/missoes/missoes.js
@@ -1,11 +1,40 @@
-let opcoes = {};
-chrome.storage.sync.get(['autoOcultarMissoes'], function (result) {
+
+let options = {};
+chrome.storage.sync.get(['ocultarMissoes'], function (result) {
     opcoes = result;
-    ocultarMissoes();
+    ocultarMissoesAddButton();
 });
 
-const ocultarMissoes = () => {
-    if (opcoes.autoOcultarMissoes) {
+const ocultarMissoesAddButton = () => {
+    $('.Missions__warnTwoMissions')
+    .append(`
+        <div>
+            <Button 
+                id="ocultarMissoesBtn" 
+                style="
+                    text-transform: capitalize; 
+                    border: 1px solid #FFFFFF;
+                    margin-left: 25px;
+                    font-size: 12px;
+                    padding: 2px 4px 2px 4px;;
+                "
+            >
+                Ocultar progressos
+            </Button>
+        </div>
+    `);
+
+    document.getElementById("ocultarMissoesBtn").addEventListener("click", function(){
+        ocultarMissoes();
+    });
+}
+
+
+function ocultarMissoes()  {
+    if ($('button:contains("Ver desafios") > svg:not(.MissionsCardBox__actionsIcon--expanded)')) {
         $('button:contains("Ver desafios") > svg:not(.MissionsCardBox__actionsIcon--expanded)').parent().click();
+    } else {
+        $('button:contains("Ver desafios") > svg:contains(.MissionsCardBox__actionsIcon--expanded)').parent().click();
     }
+
 }
