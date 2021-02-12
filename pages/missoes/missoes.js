74edbfb8a1e14ffe9330ac7de1587689a92b4382
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2021-02-12 12:22:10 -0300

    Corrige ocultar missões

--- pages/missoes/missoes.js
+++ pages/missoes/missoes.js
@@ -1,25 +1,11 @@
 let opcoes = {};
-chrome.storage.sync.get(['autoAceitarPreReady', 'autoCopiarIp', 'autoAceitarReady', 'autoConcordarTermosRanked', 'autoFixarMenuLobby', 'autoOcultarMissoes'], function (result) {
+chrome.storage.sync.get(['autoOcultarMissoes'], function (result) {
     opcoes = result;
-    initLobby();
+    ocultarMissoes();
 });
 
-const ocultarMissoes = async () => {
-    if (opcoes.autoOcultarMissoes && !($("svg")[0].hasClass("MissionsCardBox__actionsIcon--expanded"))) {
-        $('button:contains("Ver desafios")')[0].click();
+const ocultarMissoes = () => {
+    if (opcoes.autoOcultarMissoes) {
+        $('button:contains("Ver desafios") > svg:not(.MissionsCardBox__actionsIcon--expanded)').parent().click();
     }
 }
-
-(async () => {
-    $('body').on('DOMNodeInserted', '#GamersClubCSApp-missions', async function () {
-        //Wait 5 seconds before start;
-        log('Page changed, running.')
-        await new Promise(r => setTimeout(r, 3000));
-        ocultarMissoes();
-    });
-
-    ocultarMissoes();
-})();
-
-
-
