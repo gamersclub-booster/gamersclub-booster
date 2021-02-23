d7b821baf3b628a3fc88bb4980fee7be428a987c
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2021-02-23 12:32:33 -0300

    Adiciona um botão pra forçar a criação da lobby

--- pages/lobby/lobby.js
+++ pages/lobby/lobby.js
@@ -4,6 +4,9 @@ chrome.storage.sync.get(['autoAceitarPreReady', 'autoCopiarIp', 'autoAceitarRead
     initLobby();
 });
 
+let intervalCriarLobby = null
+let lobbyCriada = false
+
 const initLobby = () => {
     if (opcoes.autoAceitarPreReady) {
         let preReadyObserver = new MutationObserver((mutations) => {
@@ -17,7 +20,10 @@ const initLobby = () => {
             });
         });
 
-        preReadyObserver.observe($('#rankedModals').get(0), { childList: true, subtree: true })
+        preReadyObserver.observe($('#rankedModals').get(0), {
+            childList: true,
+            subtree: true
+        })
     }
     if (opcoes.autoCopiarIp) {
         let copyIpObserver = new MutationObserver((mutations) => {
@@ -31,7 +37,10 @@ const initLobby = () => {
             });
         });
 
-        copyIpObserver.observe($('#rankedModals').get(0), { childList: true, subtree: true })
+        copyIpObserver.observe($('#rankedModals').get(0), {
+            childList: true,
+            subtree: true
+        })
     }
     if (opcoes.autoAceitarReady) {
         let readyObserver = new MutationObserver((mutations) => {
@@ -45,7 +54,10 @@ const initLobby = () => {
             });
         });
 
-        readyObserver.observe($('#rankedModals').get(0), { childList: true, subtree: true })
+        readyObserver.observe($('#rankedModals').get(0), {
+            childList: true,
+            subtree: true
+        })
     }
     if (opcoes.autoFixarMenuLobby) {
         let observer = new MutationObserver((mutations) => {
@@ -56,22 +68,77 @@ const initLobby = () => {
                     let node = mutation.addedNodes[i]
                     if (typeof node.id != 'undefined') {
                         if (node.id.includes("SidebarSala")) {
-                            $(node).css({ position: "fixed", top: "10%", bottom: "auto" });
+                            $(node).css({
+                                position: "fixed",
+                                top: "10%",
+                                bottom: "auto"
+                            });
                         }
                         if (node.className.includes("sidebar-desafios sidebar-content")) {
-                            $(node).css({ position: 'fixed', top: '10%', right: '72px', bottom: 'auto' });
+                            $(node).css({
+                                position: 'fixed',
+                                top: '10%',
+                                right: '72px',
+                                bottom: 'auto'
+                            });
                         }
                     }
                 }
             })
         });
 
-        observer.observe($('#lobbyContent').get(0), { childList: true, subtree: true, attributes: false, characterData: false })
+        observer.observe($('#lobbyContent').get(0), {
+            childList: true,
+            subtree: true,
+            attributes: false,
+            characterData: false
+        })
     }
 
     //Auto concordar com termos da ranked.
     $('#rankedqualifyModal, #rankedopenModal, #rankedproModal, #rankedchallengeModal').on('transitionend', concordarTermos);
+
+    //Feature pra criar lobby caso full
+    $('#lobbyContent > div.row.lobby-rooms-content > div > div > div:nth-child(3)').html('<button id="forcarCriacaoLobbyBtn" style="color:orange" type="button">Forçar Criação da Lobby</button>')
+    document.getElementById("forcarCriacaoLobbyBtn").addEventListener("click", function () {
+        lobbyCriada = false;
+        intervalCriarLobby = intervalerCriacaoLobby();
+    });
 };
+//Criar lobby: https://github.com/LouisRiverstone/gamersclub-lobby_waiter/ com as modificações por causa do layout novo
+function intervalerCriacaoLobby() {
+    log('aaa');
+    return setInterval(() => {
+        if (!lobbyCriada) {
+            const lobbies = $("span.Tag__tagLabel.Tag__tagLabel--success").text().split('/');
+            //Em contato com a GC, falaram que ta variando entre 50 e 100 =(
+            if (lobbies[1] < 50) {
+                $('button.WasdButton.WasdButton--success.WasdButton--lg.LobbyHeaderButton').click()
+                
+                const antiCheat = $(".noty_bar.noty_type__info.noty_theme__mint.noty_close_with_click.noty_has_timeout.noty_close_with_button");
+                if (antiCheat.length && antiCheat.text() === "Você precisa estar com o jogo CS:GO e o Gamers Club Anti-cheat abertos para jogar. Clique aqui para baixar o GamersClub Anti-cheat.Está com o jogo e o anti-cheat abertos e ainda encontra dificuldades para jogar? Clique aqui.×") {
+                    clearInterval(intervalCriarLobby);
+                    return;
+                }
+
+                const botaoCriarSala = $(".WasdButton.WasdButton--success.WasdButton--lg.CreateLobbyModalFooterButton.CreateLobbyModalFooterButton--create");
+                if (botaoCriarSala && botaoCriarSala.text() === "Criar Sala") {
+                    //TODO: Adicionar opções de pre veto
+                    
+                    //Espera criar o modal... Verificar depois disso se criou mesmo, mas pra isso preciso testar em uma conta free quando tiver lotado....
+                    setTimeout(() => {
+                        $(".CheckboxContainer__input").click();
+                        botaoCriarSala.click();
+                        lobbyCriada = true;
+                        clearInterval(intervalCriarLobby);
+                    }, 300);
+                }
+            }
+        } else {
+            clearInterval(intervalCriarLobby)
+        }
+    }, 100)
+}
 
 function concordarTermos(e) {
     if (opcoes.autoConcordarTermosRanked && $('ranked-modal-agree').is(':visible')) {
