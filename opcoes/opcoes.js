71e6eb2b46243a8eb1ef30f844681457d19e63b4
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2021-02-27 17:59:29 -0300

    Melhorias na página de opções

--- opcoes/opcoes.js
+++ opcoes/opcoes.js
@@ -5,24 +5,54 @@ const features = [
     'autoFixarMenuLobby',
     'autoConcordarTermosRanked'
 ];
+const paginas = [
+    'geral',
+    'contato',
+    'sobre'
+]
 function iniciarPaginaOpcoes() {
+    marcarCheckboxes();
+    adicionarListenersFeatures();
+    adicionarListenersPaginas();
+}
+function marcarCheckboxes() {
     chrome.storage.sync.get(null, (response) => {
         if (!response) return false;
         for (const feature of features) {
             document.getElementById(feature).checked = response[feature];
         }
     });
-
-    adicionarListeners();
 }
-function adicionarListeners() {
+function adicionarListenersFeatures() {
     for (const feature of features) {
-        console.log(feature);
         document.getElementById(feature).addEventListener('change', function (e) {
             console.log(feature, 'changed');
             chrome.storage.sync.set({ [feature]: this.checked }, function () {});
         });
     }
+}
+
+function adicionarListenersPaginas() {
+    for (const pagina of paginas) {
+        document.getElementById(`link-${pagina}`).addEventListener('click', function (e) {
+            abrirPagina(pagina);
+        });
+    }
+}
+function abrirPagina(pagina) {
+    const link = document.getElementById(`link-${pagina}`);
+    const paginaAtiva = document.getElementById(pagina);
+    
+    Array.from(document.getElementsByClassName('ativo')).forEach((el) => {
+        el.classList.remove("ativo");
+    });
+
+    Array.from(document.getElementsByClassName('active')).forEach((el) => {
+        el.classList.remove("active");
+    });
+
+    link.classList.add('active');
+    paginaAtiva.classList.add('ativo');
 }
 
 iniciarPaginaOpcoes();
