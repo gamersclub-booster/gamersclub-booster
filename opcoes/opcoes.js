ff7fa05e5b1a6f9f3b16b82816cf9a59a56aa337
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2021-02-27 15:50:02 -0300

    Adiciona a opção de aceitar termos da ranked e refatora a página de opções

--- opcoes/opcoes.js
+++ opcoes/opcoes.js
@@ -1,33 +1,28 @@
-function constructOptions() {
-    let checkboxPreReady = document.getElementById('auto-aceitar-pre-ready');
-
-    let checkboxCopiarIp = document.getElementById('auto-copiar-ip');
-
-    let checkboxReady = document.getElementById('auto-aceitar-ready');
-
-    let checkboxFixarMenuLobby = document.getElementById('auto-fixar-menu-lobby');
-
-    chrome.storage.sync.get(null, function (result) {
-        checkboxPreReady.checked = result.autoAceitarPreReady;
-        checkboxCopiarIp.checked = result.autoCopiarIp;
-        checkboxReady.checked = result.autoAceitarReady;
-        checkboxFixarMenuLobby.checked = result.autoFixarMenuLobby;
+const features = [
+    'autoAceitarPreReady',
+    'autoCopiarIp',
+    'autoAceitarReady',
+    'autoFixarMenuLobby',
+    'autoConcordarTermosRanked'
+];
+function iniciarPaginaOpcoes() {
+    chrome.storage.sync.get(null, (response) => {
+        if (!response) return false;
+        for (const feature of features) {
+            document.getElementById(feature).checked = response[feature];
+        }
     });
 
-    checkboxPreReady.addEventListener('change', function (e) {
-        chrome.storage.sync.set({ autoAceitarPreReady: this.checked }, function () {});
-    });
-
-    checkboxCopiarIp.addEventListener('change', function (e) {
-        chrome.storage.sync.set({ autoCopiarIp: this.checked }, function () {});
-    });
-
-    checkboxReady.addEventListener('change', function (e) {
-        chrome.storage.sync.set({ autoAceitarReady: this.checked }, function () {});
-    });
-
-    checkboxFixarMenuLobby.addEventListener('change', function (e) {
-        chrome.storage.sync.set({ autoFixarMenuLobby: this.checked }, function () {});
-    });
+    adicionarListeners();
 }
-constructOptions();
+function adicionarListeners() {
+    for (const feature of features) {
+        console.log(feature);
+        document.getElementById(feature).addEventListener('change', function (e) {
+            console.log(feature, 'changed');
+            chrome.storage.sync.set({ [feature]: this.checked }, function () {});
+        });
+    }
+}
+
+iniciarPaginaOpcoes();
