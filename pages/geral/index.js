ad8370d5badaeda9d0ee1228d2c312f297f9e64e
Henrique Lauro Bagio de Souza <henriquelbsouza@gmail.com>
2021-03-31 13:05:35 -0300

    Rollback pra null ao pegar o storage

--- pages/geral/index.js
+++ pages/geral/index.js
@@ -1,7 +1,7 @@
 const log = (msg) => console.log('[GC Booster]', msg);
 
 let generalOptions = [];
-chrome.storage.sync.get(['autoMostrarLevelProgress'], function (result) {
+chrome.storage.sync.get(null, function (result) {
     generalOptions = result;
     initGcBooster();
 });
