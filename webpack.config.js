b354bc5eedecfe7fb25213a1fb92a2edc6541c72
Felipe Rugai <felipe.rugai@average.com.br>
2021-04-13 11:24:20 -0300

    finished restructuring folder tree

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
+var glob = require('glob');
 
-const contentScripts = (file) => path.resolve(__dirname, 'src', 'content-scripts', file);
-
-const options = (file) => path.resolve(__dirname, 'src', 'options', file);
+const contentScripts = (name) => glob.sync(`./src/content-scripts/${name}/*.js`);
 
 module.exports = {
-    mode: 'production',
-    entry: {
-        'index': options('index.js'),
-        'content-scripts/main': contentScripts('main.js'),
-        'content-scripts/lobby': contentScripts('lobby.js'),
-        'content-scripts/missions': contentScripts('missions.js'),
-        'content-scripts/matches': contentScripts('matches.js'),
-        'content-scripts/my-matches': contentScripts('my-matches.js'),
-        'content-scripts/profile': contentScripts('profile.js'),
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, './dist'),
-        clean: true,
-    },
-    experiments: { topLevelAwait: true },
-    plugins: [
-        new CopyPlugin({
-            patterns: ['public', 'manifest.json', 'src/options/index.html'],
-            options: {},
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-        }),
-    ],
+  mode: 'production',
+  entry: {
+    'index': glob.sync('./src/options/*.js'),
+    'content-scripts/main': contentScripts('main'),
+    'content-scripts/lobby': contentScripts('lobby'),
+    'content-scripts/missions': contentScripts('missions'),
+    'content-scripts/team': contentScripts('team'),
+    'content-scripts/my-matches': contentScripts('my-matches'),
+    'content-scripts/profile': contentScripts('profile'),
+  },
+  output: {
+    clean: true,
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true,
+  },
+  experiments: { topLevelAwait: true },
+  plugins: [
+    new CopyPlugin({
+      patterns: ['public', 'manifest.json', 'src/options/index.html'],
+      options: {},
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
 };
