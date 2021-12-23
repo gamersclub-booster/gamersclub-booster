77ed97c29a552d45cce54e218985049ba4aeac65
Joao Victor Davim <joaovictor_rj@poli.ufrj.br>
2021-12-23 14:26:26 -0300

    feat: ajusta posicao e comportamento do lobby sidebar

--- src/content-scripts/lobby/autoFixarMenuLobby.js
+++ src/content-scripts/lobby/autoFixarMenuLobby.js
@@ -12,32 +12,30 @@ export const autoFixarMenuLobby = mutations =>
           const node = mutation.addedNodes[i];
           if ( typeof node.id !== 'undefined' ) {
             if ( node.id.includes( 'SidebarSala' ) ) {
-              if ( !isSubscriber ) {
-                $( node ).css( {
-                  position: 'fixed',
-                  top: '130px',
-                  bottom: 'auto'
-                } );
-              } else {
-                $( node ).css( {
-                  position: 'fixed',
-                  top: '10%',
-                  bottom: 'auto'
-                } );
-              }
+              $ ( node ).css( {
+                'position': 'absolute',
+                'left': '0',
+                'top': '33px',
+                'width': '227px',
+                'border-radius': '0 6px 6px 0',
+                'background': '#2c2d3d',
+                'z-index': '4',
+                'paddin': '12px 8px',
+                'bottom': 'auto'
+              } );
             }
             if ( node.className.includes( 'sidebar-desafios sidebar-content' ) ) {
               if ( !isSubscriber ) {
                 $( node ).css( {
                   position: 'fixed',
-                  top: '130px',
+                  top: '22%',
                   right: '72px',
                   bottom: 'auto'
                 } );
               } else {
                 $( node ).css( {
                   position: 'fixed',
-                  top: '10%',
+                  top: '20%',
                   right: '72px',
                   bottom: 'auto'
                 } );
