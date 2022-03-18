27e327defe75f8e8da8a2adf50ee506d0914b223
Leandro Ribeiro <leandro98.ata@gmail.com>
2022-03-18 11:45:58 -0300

    feat: torna como lido o link do Daily Rewards no menu ao fazer a coleta

--- src/content-scripts/main/autoDailyRewards.js
+++ src/content-scripts/main/autoDailyRewards.js
@@ -10,6 +10,15 @@ export const coletarDailyRewards = async () => {
     'x-product-session': `${productSession}`
   };
 
-  await axios.post( `https://missions-api.${ GC_URL }/player/daily-rewards/claim`, '', { headers } );
+  await axios.post( `https://missions-api.${ GC_URL }/player/daily-rewards/claim`, '', { headers } ).then(
+    () => {
+      localStorage.setItem( 'daily_rewards_claim_date', new Date().toISOString() );
+
+      const dailyRewardsBtn = document.querySelector( '[href="/daily-rewards"]' );
+      dailyRewardsBtn?.querySelector( 'span.MainMenu__itemNewsIcon' ).remove();
+      dailyRewardsBtn?.querySelector( '.MainMenu__itemLabel' )
+        .classList.remove( 'MainMenu__itemLabel--hasNews' );
+    } );
+
   chrome.storage.sync.set( { lastCollectedDailyRewardsTs: Date.now() } );
 };
