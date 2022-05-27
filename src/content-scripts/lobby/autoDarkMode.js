0b216210ee5c89a733db0f3749ad5e2ddb328050
Jorge Brunetto <jorgebrunetto@hotmail.com>
2022-05-27 14:01:53 -0300

    Update autoDarkMode.js
    Adicionado o dark mode em todos os itens.

--- src/content-scripts/lobby/autoDarkMode.js
+++ src/content-scripts/lobby/autoDarkMode.js
@@ -2,6 +2,8 @@ export const autoDarkMode = () =>
   chrome.storage.sync.get( [ 'autoDarkMode' ], function ( result ) {
     if ( result.autoDarkMode ) {
       $( '.lobby-panel-content-info, .LobbyHeader, .Topbar__freeBg' ).css( 'background', 'black' );
-      $( ' .GamersClubCSApp, .GamersClubApp, .LobbyHeader, .MainHeader' ).css( 'background', 'black' );
+      $( '.GamersClubCSApp, .GamersClubApp, .LobbyHeader, .MainHeader' ).css( 'background', 'black' );
+      $( '.MainHeader__logo, .footer, .no-padding.sitemap, body, .SettingsMenu, .gcf-room-list-scroll, .gcf-menu' ).css( 'background', 'black' );
     }
   } );
+
