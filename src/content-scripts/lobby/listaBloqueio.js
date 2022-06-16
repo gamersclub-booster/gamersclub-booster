f4c71f44dcea0712846350b83b4d6cba33dddfb4
Jorge Brunetto <jorgebrunetto@hotmail.com>
2022-06-16 08:11:43 -0300

    feat: adicionado KDR na sala

--- src/content-scripts/lobby/listaBloqueio.js
+++ src/content-scripts/lobby/listaBloqueio.js
@@ -1,5 +1,4 @@
 import { alertaMsg } from '../../lib/messageAlerts';
-import { levelColor } from '../../lib/constants';
 
 export const listaBloqueio = mutations =>
   chrome.storage.sync.get( [ 'blockList' ], function ( ) {
@@ -17,25 +16,6 @@ export const listaBloqueio = mutations =>
 
               const id = selectorLink.getAttribute( 'href' ).replace( '/jogador/', '' );
               const nick = selectorLink.getAttribute( 'title' ).split( ' | ' )[0];
-              const kdr = selectorLink.getAttribute( 'title' ).split( ' | ' )[1];
-
-              const searchKdr = kdr.split( ' ' )[1];
-
-              const colorKrdDefault = searchKdr <= 2 ? '#000' :
-                'linear-gradient(135deg, rgba(0,255,222,0.8) 0%, rgba(245,255,0,0.8) 30%, rgba(255,145,0,1) 60%, rgba(166,0,255,0.8) 100%)';
-              const colorKdr = searchKdr <= 2 ? levelColor[Math.round( searchKdr * 10 )] : colorKrdDefault;
-
-              const kdrDiv = document.createElement( 'span' );
-              kdrDiv.style = `background: ${colorKdr};padding:2px 4px;position:absolute;top:0;right:0;z-index:5;font-size:11px;`;
-              kdrDiv.innerHTML = searchKdr;
-
-              // node.querySelector( '.sidebar-item-meta ' ).append( `${searchKdr}` );
-              // node.querySelector( '.sidebar-item-meta ' ).append( $( `div style="'background-color': ${colorKdr}">${searchKdr}</div> ` ) );
-              node.querySelector( '.sidebar-item-meta ' ).append( kdrDiv );
-
-              // console.log( searchKdr, 'searchKdr' );
-              // console.log( 'node', node );
-              // console.log( 'Entrou o ID ' + id + ' nick: ' + nick );
               if ( res.blockList.includes( id ) ) {
                 alertaMsg( prefix + ': Essa pessoa: ' + nick + ' está na sua lista de bloqueio' );
               }
