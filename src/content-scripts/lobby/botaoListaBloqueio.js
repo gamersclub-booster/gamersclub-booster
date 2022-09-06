c62fab167f31348bfd94ff5cb685569972da0732
Fernando Barboza <falandocomofernando@gmail.com>
2022-09-05 21:56:31 -0300

    feat: textos dos botões de ação de acordo com a linguagem setada na extensão

--- src/content-scripts/lobby/botaoListaBloqueio.js
+++ src/content-scripts/lobby/botaoListaBloqueio.js
@@ -1,5 +1,5 @@
 import { adicionarNaLista, removerDaLista } from '../../lib/blockList';
-import { alertaMsg } from '../../lib/messageAlerts';
+import { getAllStorageSyncData, getTranslationText } from '../../utils';
 
 export function initListaBloqueio() {
   chrome.storage.sync.get( [ 'blockList' ], function ( result ) {
@@ -12,6 +12,13 @@ export function initListaBloqueio() {
 }
 
 async function initBotaoListaBloqueio() {
+  const { traducao } = await getAllStorageSyncData();
+  const addBlocklistText = getTranslationText( 'adicionar-a-lista-de-bloqueio', traducao );
+  const removeBlocklistText = getTranslationText( 'remover-da-lista-de-bloqueio', traducao );
+  const addedBlocklistText = getTranslationText( 'foi-adicionado-a-lista-bloqueio', traducao );
+  const removedBlocklistText = getTranslationText( 'foi-removido-da-lista-bloqueio', traducao );
+  const seuNickReal = document.getElementsByClassName( 'MainHeader__playerNickname' )[0].innerText;
+
   //Quando iniciar, adicionar os botoes da lista de bloqueio
   const playerSelector = $( '.tableMatch__leftColumn' );
   for ( let i = 0; i < playerSelector.length; i++ ) {
@@ -25,38 +32,36 @@ async function initBotaoListaBloqueio() {
       const botaoLista = document.getElementsByClassName( 'botaoListaDeBloqueio' );
       for ( let i = 0; i < botaoLista.length; i++ ) {
         //Verificar se já existe no array
-        const nick = botaoLista[i].offsetParent.children[0].children[0].children[2].innerText;
         const id = botaoLista[i].offsetParent.children[0].children[0].children[2].href.replace( 'https://gamersclub.com.br/jogador/', '' );
         const avatarURL = botaoLista[i].offsetParent.children[0].children[0].children[0].children[1].currentSrc;
 
         const listaDeTodosOsIDs = result.blockList.map( e => { return e.id; } );
         if ( listaDeTodosOsIDs.includes( id ) ) {
-          botaoLista[i].innerText = 'Remover da lista de bloqueio';
+          botaoLista[i].innerText = removeBlocklistText;
           botaoLista[i].setAttribute( 'data', 'alreadyListed' );
         } else {
-          botaoLista[i].innerText = 'Adicionar a lista de bloqueio';
+          botaoLista[i].innerText = addBlocklistText;
           botaoLista[i].setAttribute( 'data', 'notAlreadyListed' );
         }
         //Adicionar o listener de clique
         botaoLista[i].addEventListener( 'click', function ( click ) {
-          const prefix = '<a style="color: yellow;">[ Lista de Bloqueio ]</a>';
           const state = botaoLista[i].attributes[0].value;
 
           if ( state === 'alreadyListed' ) {
             //Remover da lista
-            removerDaLista( { id, nick, avatarURL }, function ( ) {
-              const nickName = `<a style='color: yellow;'>${click.path[1].outerText.split( '\n' )[0]}</a>`;
-              botaoLista[i].innerText = 'Adicionar a lista de bloqueio';
+            removerDaLista( { id, avatarURL }, function ( ) {
+              const nickName = click.path[1].outerText.split( '\n' )[0];
+              botaoLista[i].innerText = addBlocklistText;
               botaoLista[i].setAttribute( 'data', 'notAlreadyListed' );
-              alertaMsg( prefix + ' - Removido o(a) ' + nickName + ' na sua lista de bloqueio.' );
+              alert( `\n${nickName} ${removedBlocklistText}.` );
             } );
           } else {
             //Adicionar a lista
-            adicionarNaLista( { id, nick, avatarURL }, function ( ) {
-              const nickName = `<a style='color: yellow;'>${click.path[1].outerText.split( '\n' )[0]}</a>`;
-              botaoLista[i].innerText = 'Remover da lista de bloqueio';
+            adicionarNaLista( { id, avatarURL }, function ( ) {
+              const nickName = click.path[1].outerText.split( '\n' )[0];
+              botaoLista[i].innerText = removeBlocklistText;
               botaoLista[i].setAttribute( 'data', 'alreadyListed' );
-              alertaMsg( prefix + ' - Adicionado o(a) ' + nickName + ' na sua lista de bloqueio.' );
+              alert( `\n${nickName} ${addedBlocklistText}.` );
             } );
           }
         } );
