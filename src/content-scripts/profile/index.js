eb4f83d454aa044ded9458bd24b2be01535e3161
Eduardo Mathioni <78946931+EMathioni@users.noreply.github.com>
2022-10-10 17:04:05 -0300

    fix: fixing kill/death/(diff|kdr) visualization of current month in an alternative link

--- src/content-scripts/profile/index.js
+++ src/content-scripts/profile/index.js
@@ -1,11 +1,25 @@
 const SELETOR_HISTORICO = 'h3:contains("Histórico")';
-const BASE_URL = 'https://gamersclub.com.br';
+const LOCAT = $( location ).attr( 'href' );
+const MAIN_URL = 'https://gamersclub.com.br';
+const ALT_URL = 'https://csgo.gamersclub.gg';
+
+const urlFormatter = () => {
+  if ( LOCAT.includes( `${ALT_URL}/player` ) ) {
+    return `${MAIN_URL}/player/${LOCAT.substring( 34 )}`;
+  } else if ( LOCAT.includes( `${ALT_URL}/jogador` ) ) {
+    return `${MAIN_URL}/jogador/${LOCAT.substring( 35 )}`;
+  } else {
+    return LOCAT;
+  }
+};
 
 const initProfilePage = () => {
   let totalVitorias = 0;
   let totalDerrotas = 0;
-  const idPlayer = $( location ).attr( 'href' ).replace( `${ BASE_URL }/player/`, '' ).
-    replace( `${ BASE_URL }/jogador/`, '' );
+  const idPlayer = urlFormatter().includes( 'player' ) ?
+    urlFormatter().replace( `${MAIN_URL}/player/`, '' ) :
+    urlFormatter().replace( `${MAIN_URL}/jogador/`, '' );
+
   $( 'span:contains(\'Vitórias\')' ).each( function () {
     totalVitorias += parseInt( $( this ).html().replace( ' Vitórias', '' ) );
   } );
@@ -31,4 +45,3 @@ const initProfilePage = () => {
 };
 
 initProfilePage();
-
