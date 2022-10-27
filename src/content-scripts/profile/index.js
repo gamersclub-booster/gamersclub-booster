ee3eca609ef0f806dcd2a8ddd9e5bd01e07be727
Eduardo Mathioni <78946931+EMathioni@users.noreply.github.com>
2022-10-27 18:25:25 -0300

    fix: code improvement

--- src/content-scripts/profile/index.js
+++ src/content-scripts/profile/index.js
@@ -21,7 +21,7 @@ const initProfilePage = () => {
   } );
 
   $.getJSON( {
-    dataType: 'json',
+    data: 'json',
     url: `https://${GC_URL}/api/box/history/${idPlayer}`
   } ).done( function ( json ) {
     const totalKillsMes = json.stat[2].value;
@@ -29,34 +29,24 @@ const initProfilePage = () => {
     const diffKills = totalKillsMes - totalMortesMes;
     const totalKdrMes = json.stat[0].value;
     const winRatio = ( ( totalVitorias / ( totalVitorias + totalDerrotas ) ) * 100 ).toFixed( 2 );
-    const styledWinRatio = winRatio >= 50 ?
-      `<span style=color:green>${winRatio}</span>` : `<span style=color:red>${winRatio}</span>`;
 
-    const styledDiffKills = () => {
-      if ( diffKills > 0 ) {
-        return `<span style=color:green>+${diffKills}</span>`;
-      } else if ( diffKills < 0 ) {
-        return `<span style=color:red>${diffKills}</span>`;
+    const calcColor = ( value, base ) => {
+      if ( value > base ) {
+        return 'green';
+      } else if ( value < base ) {
+        return 'red';
       } else {
-        return `<span style=color:yellow>${diffKills}</span>`;
+        return 'yellow';
       }
     };
 
-    const styledKdrMes = () => {
-      if ( totalKdrMes >= 1 ) {
-        return `<span style=color:green>${totalKdrMes}</span>`;
-      } else if ( totalKdrMes < 1 ) {
-        return `<span style=color:red>${totalKdrMes}</span>`;
-      } else {
-        return `<span style=color:yellow>${totalKdrMes}</span>`;
-      }
-    };
     const titleHistorico = $( SELETOR_HISTORICO )[0];
     titleHistorico.innerHTML += ` -
     ${totalVitorias} Vitórias / ${totalDerrotas} Derrotas (${totalPartidas} Partidas | 
-      ${styledWinRatio}% Win Rate) <br>
+    <span style=color:${calcColor( winRatio, 50 )}>${winRatio}</span>% Win Rate) <br>
     Este mês: ${totalKillsMes} Kills / ${totalMortesMes}
-    Mortes (${styledDiffKills()} | KDR: ${styledKdrMes()})`;
+    Mortes (<span style=color:${calcColor( diffKills, 0 )}>${diffKills > 0 ? `+${diffKills}` : diffKills}</span> | 
+    KDR: <span style=color:${calcColor( totalKdrMes, 1 )}>${totalKdrMes}</span>)`;
   } );
 
 };
