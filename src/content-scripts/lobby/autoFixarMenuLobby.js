428adc998c29ea4b81dfa88e247a14ec5e3a8752
Jorge Brunetto <jorgebrunetto@hotmail.com>
2023-01-09 21:37:04 -0300

    fix: melhoria no fixar desafios

--- src/content-scripts/lobby/autoFixarMenuLobby.js
+++ src/content-scripts/lobby/autoFixarMenuLobby.js
@@ -1,45 +1,14 @@
-import { retrieveWindowVariables } from '../../lib/dom';
-
 export const autoFixarMenuLobby = mutations =>
   chrome.storage.sync.get( [ 'autoFixarMenuLobby' ], function ( result ) {
     if ( result.autoFixarMenuLobby ) {
-      const windowVariables = retrieveWindowVariables( [ 'ISSUBSCRIBER' ] );
-      const isSubscriber = windowVariables.ISSUBSCRIBER;
       mutations.forEach( mutation => {
         if ( !mutation.addedNodes ) { return; }
 
         for ( let i = 0; i < mutation.addedNodes.length; i++ ) {
           const node = mutation.addedNodes[i];
           if ( typeof node.id !== 'undefined' ) {
-            if ( node.id.includes( 'SidebarSala' ) ) {
-              $ ( node ).css( {
-                'position': 'fixed',
-                'left': '0',
-                'top': '160px',
-                'width': '227px',
-                'border-radius': '0 6px 6px 0',
-                'background': '#2c2d3d',
-                'z-index': '4',
-                'paddin': '12px 8px',
-                'bottom': 'auto'
-              } );
-            }
             if ( node.className.includes( 'sidebar-desafios sidebar-content' ) ) {
-              if ( !isSubscriber ) {
-                $( node ).css( {
-                  position: 'fixed',
-                  top: '22%',
-                  right: '72px',
-                  bottom: 'auto'
-                } );
-              } else {
-                $( node ).css( {
-                  position: 'fixed',
-                  top: '20%',
-                  right: '72px',
-                  bottom: 'auto'
-                } );
-              }
+              $( node ).parent().addClass( 'with-fixed-menu' );
             }
           }
         }
