456d189a1278f4aa0c3dd59273159cb2b12fe6a1
Luiz Fernando <luizfernandosv85@gmail.com>
2023-06-26 18:07:35 -0300

    adicionar coverage em testes!

--- src/lib/maps.test.js
+++ src/lib/maps.test.js
@@ -3,4 +3,5 @@ import { getMapImage } from "./maps";
 
 it ('seleciona mapas', () =>{
     expect(getMapImage('de_mirage')).toBe('https://steamuserimages-a.akamaihd.net/ugc/1822269383661554784/0F9E7C44F50C692C1EEC1FC677CFDD3EF646F205/')
+
 })
