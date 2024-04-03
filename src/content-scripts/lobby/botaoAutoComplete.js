9f6eb818977fefc22d89900cd5ccb135b0305ac4
Henrique Lauro Bagio de Souza <henrique.souza@meltwater.com>
2024-04-03 18:00:29 -0300

    fix: remove setTimeout da opção de completar partida

--- src/content-scripts/lobby/botaoAutoComplete.js
+++ src/content-scripts/lobby/botaoAutoComplete.js
@@ -2,7 +2,6 @@ import { alertaMsg } from '../../lib/messageAlerts';
 import { getAllStorageSyncData, getTranslationText } from '../../utils';
 import { preVetosMapas } from '../../lib/constants';
 
-let interval = 1000;
 let intervalId;
 
 export async function adicionarBotaoAutoComplete() {
@@ -73,44 +72,38 @@ export async function adicionarBotaoAutoComplete() {
 async function intervalerAutoComplete() {
   intervalId = setInterval( async function () {
     if ( !$( '#SidebarSala' ).length ) { // Se não estiver em lobby ( acontece quando cria lobby e já está buscando complete )
-      interval = randomIntFromInterval( 750, 4750 ); // Escolhe um novo intervalo aleatório entre 1s e 5s
       chrome.storage.sync.get( [ 'complete', 'roundsDiff', 'roundsMin' ], async res => {
         if ( $( '.scroll-content > li > .btn-actions > a.accept-btn' ).length ) {
-          setTimeout( function () { // Espera tempo aleatório entre 1 e 5 segundos
-            const score1 = $( '.scroll-content > li > .match-info > .result-type > .score > span' ).eq( 0 ).text();
-            const score2 = $( '.scroll-content > li > .match-info > .result-type > .score > span' ).eq( 1 ).text();
-            const mapCode = getMapCode( $( '.map-name' ).text() );
+          const score1 = $( '.scroll-content > li > .match-info > .result-type > .score > span' ).eq( 0 ).text();
+          const score2 = $( '.scroll-content > li > .match-info > .result-type > .score > span' ).eq( 1 ).text();
+          const mapCode = getMapCode( $( '.map-name' ).text() );
 
-            if ( score1 && score2 && mapCode ) {
-              let isInCheckedMaps = true, hasMinRounds = true, isInDiff = true;
+          if ( score1 && score2 && mapCode ) {
+            let isInCheckedMaps = true, hasMinRounds = true, isInDiff = true;
 
-              if ( res.complete ) {
-                isInCheckedMaps = !res.complete.length || res.complete.includes( mapCode );
-              }
-
-              if ( res.roundsMin ) {
-                hasMinRounds = ( parseInt( score1 ) + parseInt( score2 ) ) >= res.roundsMin;
-              }
-
-              if ( res.roundsDiff ) {
-                isInDiff = Math.abs( score1 - score2 ) <= res.roundsDiff;
-              }
-
-              if ( isInCheckedMaps && isInDiff && hasMinRounds ) {
-                $( '.scroll-content > li > .btn-actions > a.accept-btn' ).get( 0 ).click();
-                $( '#completePlayerModal > div > div.buttons > button.sm-button-accept.btn.btn-success' ).get( 0 ).click();
-              }
+            if ( res.complete ) {
+              isInCheckedMaps = !res.complete.length || res.complete.includes( mapCode );
             }
-          }, interval );
+
+            if ( res.roundsMin ) {
+              hasMinRounds = ( parseInt( score1 ) + parseInt( score2 ) ) >= res.roundsMin;
+            }
+
+            if ( res.roundsDiff ) {
+              isInDiff = Math.abs( score1 - score2 ) <= res.roundsDiff;
+            }
+
+            if ( isInCheckedMaps && isInDiff && hasMinRounds ) {
+              $( '.scroll-content > li > .btn-actions > a.accept-btn' ).get( 0 ).click();
+              $( '#completePlayerModal > div > div.buttons > button.sm-button-accept.btn.btn-success' ).get( 0 ).click();
+            }
+          }
         }
       } );
     } else {
       clearInterval( intervalId );
     }
   }, 250 );
-}
-function randomIntFromInterval( min, max ) {
-  return Math.floor( ( Math.random() * ( max - min + 1 ) ) + min );
 }
 
 function getMapCode( mapName ) {
