206a058d8ef3c498e08e4d282ec76cbacc7d5d25
Henrique Lauro Bagio de Souza <henrique.souza@meltwater.com>
2024-04-03 12:08:27 -0300

    fix: alertas não funcionam WIP

--- src/lib/messageAlerts.js
+++ src/lib/messageAlerts.js
@@ -4,6 +4,7 @@ export function alertaMsg( msg ) {
   if ( isFirefox ) {
     window.wrappedJSObject.errorAlert( msg );
   } else {
-    location.href = `javascript:errorAlert('${msg}'); void 0`;
+    // location.href = `javascript:errorAlert('${msg}'); void 0`; // Essa linha ta bloqueada, antes mostrava a msg certinho no padrão da GC, com o toaster lá no canto... n faço ideia de como resolver
+    window.alert( msg ); // esse alerta é uma merda.
   }
 }
