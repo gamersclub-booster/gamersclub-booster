7ab546fcd9ee44c274b722dafbeb70ec470fa8c4
Henrique Souza <henrique.souza@meltwater.com>
2024-10-30 14:34:52 -0300

    fix: corrige o som de ready com intervaler, porém ainda toca o bip da gc

--- src/content-scripts/lobby/somReady.js
+++ src/content-scripts/lobby/somReady.js
@@ -3,9 +3,10 @@ export const somReady = mutations =>
     if ( result.somReady ) {
       $.each( mutations, ( _i, mutation ) => {
         const addedNodes = $( mutation.addedNodes );
-        const selector = 'button:contains(\'Ready\')';
+        //eslint-disable-next-line
+        const selector = "button:contains('Ready')";
         const readyButton = addedNodes.find( selector ).addBack( selector );
-        if ( readyButton && readyButton.length && readyButton.text() === 'Ready' && !readyButton.disabled ) {
+        if ( readyButton && readyButton.length && !readyButton.disabled ) {
           const som = result.somReady === 'custom' ? result.customSomReady : result.somReady;
           const audio = new Audio( som );
           const volume = result.volume || 100;
@@ -15,3 +16,21 @@ export const somReady = mutations =>
       } );
     }
   } );
+
+export function somReadySetInterval() {
+  setInterval( async () => {
+    chrome.storage.sync.get( [ 'somReady', 'customSomReady', 'volume' ], function ( result ) {
+      if ( result.somReady ) {
+        // eslint-disable-next-line
+        const readyButton = $( "button:contains('Ready')" );
+        if ( readyButton && readyButton.length && !readyButton.disabled ) {
+          const som = result.somReady === 'custom' ? result.customSomReady : result.somReady;
+          const audio = new Audio( som );
+          const volume = result.volume || 100;
+          audio.volume = volume / 100;
+          $( readyButton ).on( 'click', function () { audio.play(); } );
+        }
+      }
+    } );
+  }, 150 );
+}
