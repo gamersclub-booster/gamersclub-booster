2a11db92880730493dc89423e333cc66fb8ceed0
Paulo Junior <73448096+N7z@users.noreply.github.com>
2025-09-29 18:49:00 -0300

    fix: eslint on auto copy lobby link

--- src/content-scripts/lobby/autoCopyLobbyLink.js
+++ src/content-scripts/lobby/autoCopyLobbyLink.js
@@ -3,39 +3,40 @@ import { GC_URL } from '../../lib/constants';
 import { alertaMsg } from '../../lib/messageAlerts';
 
 export const autoCopyLobbyLink = mutations => {
-  chrome.storage.sync.get(['autoCopyLobbyLink'], result => {
-    if (!result.autoCopyLobbyLink) {
+  chrome.storage.sync.get( [ 'autoCopyLobbyLink' ], result => {
+    if ( !result.autoCopyLobbyLink ) {
       return;
     }
 
-    mutations.forEach(async mutation => {
-      if (!mutation.addedNodes) {
+    mutations.forEach( async mutation => {
+      if ( !mutation.addedNodes ) {
         return;
       }
 
-      for (let i = 0; i < mutation.addedNodes.length; i++) {
+      for ( let i = 0; i < mutation.addedNodes.length; i++ ) {
         const node = mutation.addedNodes[i];
-        
+
         if (
           node.nextElementSibling &&
           node.nextElementSibling.className &&
-          node.nextElementSibling.className.includes('MyRoom')
+          node.nextElementSibling.className.includes( 'MyRoom' )
         ) {
           try {
-            const lobbyInfo = await axios.post(`https://${GC_URL}/lobbyBeta/openRoom`);
+            const lobbyInfo = await axios.post( `https://${GC_URL}/lobbyBeta/openRoom` );
             const lobbyId = lobbyInfo?.data?.lobby?.lobbyID;
             const password = lobbyInfo?.data?.lobby?.password || '';
-            const url = lobbyId ? `https://${GC_URL}/j/${lobbyId}/${password}` + '?utm_source=lobby&utm_medium=invite&utm_campaign=user_invitation' : null;
+            const url = lobbyId ?
+              `https://${GC_URL}/j/${lobbyId}/${password}` + '?utm_source=lobby&utm_medium=invite&utm_campaign=user_invitation' : null;
 
-            if (!url) { throw new Error('Lobby URL não disponível'); }
+            if ( !url ) { throw new Error( 'Lobby URL não disponível' ); }
 
-            await navigator.clipboard.writeText(url);
-            alertaMsg('[GC Booster] - Link da lobby copiado automaticamente!');
-          } catch (error) {
-            console.error('Erro ao obter/copiar link da lobby:', error);
+            await navigator.clipboard.writeText( url );
+            alertaMsg( '[GC Booster] - Link da lobby copiado automaticamente!' );
+          } catch ( error ) {
+            console.error( 'Erro ao obter/copiar link da lobby:', error );
           }
         }
       }
-    });
-  });
+    } );
+  } );
 };
